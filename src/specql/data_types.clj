61a6e00212bd387fe62ff76538ad21f9b01fd3be
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-03-07 13:42:13 +0200

    Add timestamp spec

--- src/specql/data_types.clj
+++ src/specql/data_types.clj
@@ -9,6 +9,8 @@
 ;; Postgres can handle upto 4713 BC, but do we need it?
 (s/def ::date (s/inst-in #inst "0001-01-01T00:00:00.000-00:00"
                          #inst "9999-12-31T23:59:59.999-00:00"))
+(s/def ::timestamp ::date)
+
 (s/def ::numeric bigdec?)
 (s/def ::text string?)
 (s/def ::bool boolean?)

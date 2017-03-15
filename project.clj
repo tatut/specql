59bc17e9c504a54c4deb37c5e3d5bfcb9e2eed89
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-03-15 08:24:42 +0200

    Upgrade to latest 1.9 alpha

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject specql/specql "0.4-SNAPSHOT"
   :description "PostgreSQL spec query language"
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
                  [org.clojure/java.jdbc "0.7.0-alpha2"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
                                  [com.opentable.components/otj-pg-embedded "0.7.1"]]}}

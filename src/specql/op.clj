2cfe41e6772a9b4c8d0d40587867aa41accd7bd9
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-05-07 13:31:57 +0300

    Operators now take column-info
    Operators need the column info in order to do transformations and add
    type casts if necessary.

--- src/specql/op.clj
+++ src/specql/op.clj
@@ -1,29 +1,47 @@
 (ns specql.op
   "SQL operators"
   (:refer-clojure :exclude [< > <= >= = not= not or and])
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [specql.transform :as xf]))
 
 (defprotocol Op
-  (to-sql [this value-accessor]))
+  (to-sql [this value-accessor column-info]
+    "Convert the operator to SQL, must return a vector containing
+  a SQL fragment and possible parameters. The column-info provides the
+  introspected information about the column, like its type and possible
+  transformation to apply."))
+
+(defn- expand-sql-template [sql-template val arg param]
+  (for [s sql-template]
+    (case s
+      :val val
+      :arg param
+      :args `(str/join "," (repeat (count (first ~arg)) param))
+      s)))
 
 (defmacro defoperator [sym sql-template]
   (let [optype (symbol (str "Op" (name sym)))
         optype-cons (symbol (str "->Op" (name sym)))
         val (gensym "val")
-        arg (gensym "arg")]
+        arg (gensym "arg")
+        param (gensym "param")]
     `(do
        (defrecord ~optype [~arg]
          Op
-         (to-sql [_# ~val]
-           [(str ~@(for [s sql-template]
-                     (case s
-                       :val val
-                       :arg "?"
-                       :args `(str/join "," (repeat (count (first ~arg)) "?"))
-                       s)))
-            ~(if (some #(clojure.core/= :args %) sql-template)
-               `(vec (first ~arg))
-               arg)]))
+         (to-sql [_# ~val column-info#]
+           (let [transform# (::xf/transform column-info#)
+                 type# (:type column-info#)
+                 ~param (if (:enum? column-info#)
+                          (str "?::" type#)
+                          "?")
+                 ~arg (if transform#
+                        (map (fn [a#]
+                               (xf/to-sql transform# a#)) ~arg)
+                        ~arg)]
+             [(str ~@(expand-sql-template sql-template val arg param))
+              ~(if (some #(clojure.core/= :args %) sql-template)
+                 `(vec (first ~arg))
+                 arg)])))
        (defn ~sym [& arg#]
          (~optype-cons arg#)))))
 
@@ -38,13 +56,19 @@
 
 (defrecord Inop [values]
   Op
-  (to-sql [_ v]
-    [(str v " IN ("
-          (if (empty? values)
-            "NULL"
-            (str/join "," (repeat (count values) "?")))
-          ")")
-     (vec values)]))
+  (to-sql [_ v {transform ::xf/transform type :type enum? :enum? :as column-info}]
+    (let [values (if transform
+                   (map #(xf/to-sql transform %) values)
+                   values)
+          param (if enum?
+                  (str "?::" type)
+                  "?")]
+      [(str v " IN ("
+            (if (empty? values)
+              "NULL"
+              (str/join "," (repeat (count values) param)))
+            ")")
+       (vec values)])))
 
 (defn in [values]
   (assert (clojure.core/and
@@ -56,18 +80,18 @@
 
 (def null?
   (reify Op
-    (to-sql [_ v]
+    (to-sql [_ v c]
       [(str v " IS NULL") []])))
 
 (def not-null?
   (reify Op
-    (to-sql [_ v]
+    (to-sql [_ v c]
       [(str v " IS NOT NULL") []])))
 
 (defrecord Opnot [op]
   Op
-  (to-sql [_ value-accessor]
-    (let [[sql params] (to-sql op value-accessor)]
+  (to-sql [_ value-accessor column-info]
+    (let [[sql params] (to-sql op value-accessor column-info)]
       [(str "NOT (" sql ")")
        params])))
 
@@ -76,13 +100,13 @@
 
 (defrecord CombinedOp [combine-with ops]
   Op
-  (to-sql [_ value-accessor]
+  (to-sql [_ value-accessor column-info]
     (loop [sql []
            params []
            [op & ops] (remove nil? ops)]
       (if-not op
         [(str "(" (str/join combine-with sql) ")") params]
-        (let [[op-sql op-params] (to-sql op value-accessor)]
+        (let [[op-sql op-params] (to-sql op value-accessor column-info)]
           (recur (conj sql op-sql)
                  (into params op-params)
                  ops))))))

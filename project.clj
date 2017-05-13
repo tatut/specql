de5b76d4f90b96852f748d6922774ac9fbebaa4a
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-05-13 11:45:55 +0300

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject specql/specql "0.6-alpha6"
+(defproject specql/specql "0.6-alpha7"
   :description "PostgreSQL spec query language"
   :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
                  [org.clojure/java.jdbc "0.7.0-alpha3"]]

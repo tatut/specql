205d5f44383d7fcff66e6d16a093808fcaf3b6c5
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-05-17 15:49:56 +0300

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject specql/specql "0.6-alpha16"
+(defproject specql/specql "0.6-alpha17"
   :description "PostgreSQL spec query language"
   :license {:name "MIT License"}
   :url "https://tatut.github.io/specql/"

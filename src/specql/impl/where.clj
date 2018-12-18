010e4289c83770cb57f79e3aed718906fbfa4fe1
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-12-18 19:50:54 +0200

    Clarify threaded form

--- src/specql/impl/where.clj
+++ src/specql/impl/where.clj
@@ -43,51 +43,55 @@
            :parameters (conj parameters
                              (transform-value-to-sql column (assert-spec column-keyword value))))))
 
+(defn- return-sql-and-parameters [where-clause]
+  (as-> where-clause w
+      (update w :clause #(str/join " AND " %))
+      ((juxt :clause :parameters) w)))
+
 (defn- where-map [table-info-registry path->table record path-prefix]
   (let [{:keys [table alias]} (path->table path-prefix)
         table-columns (-> table table-info-registry :columns)]
-    (as-> (reduce
-           (fn [where [column-keyword value]]
-             ;; If column is a joined table, it has a mapping in path->table.
-             ;; Recursively create clauses for the value
-             (if (path->table (into path-prefix [column-keyword]))
-               (let [[sql params] (sql-where table-info-registry path->table value
-                                             (into path-prefix [column-keyword])) ]
-                 (assoc where
-                        :clause (conj (:clause where) (str "(" sql ")"))
-                        :parameters (into (:parameters where) params)))
+    (return-sql-and-parameters
+     (reduce
+      (fn [where [column-keyword value]]
+        ;; If column is a joined table, it has a mapping in path->table.
+        ;; Recursively create clauses for the value
+        (if (path->table (into path-prefix [column-keyword]))
+          (let [[sql params] (sql-where table-info-registry path->table value
+                                        (into path-prefix [column-keyword])) ]
+            (assoc where
+                   :clause (conj (:clause where) (str "(" sql ")"))
+                   :parameters (into (:parameters where) params)))
 
-               ;; This is a column in the current table
-               (let [{col-name :name :as column} (column-keyword table-columns)]
-                 (assert column
-                         (str "Unknown column in where clause: no "
-                              column-keyword " in table " table))
-                 (if-let [composite-columns (and
-                                             (map? value)
-                                             (some->> column :type
-                                                      (composite-type table-info-registry)
-                                                      table-info-registry
-                                                      :columns))]
-                   ;; composite type: add all fields as separate clauses
-                   (reduce (fn [where [kw val]]
-                             (assert (composite-columns kw)
-                                     (str "Unknown column in where clause: no "
-                                          kw " in composite type "
-                                          (composite-type table-info-registry (:type column))))
-                             (add-where where
-                                        (composite-columns kw)
-                                        (str "(" alias ".\"" col-name "\").\""
+          ;; This is a column in the current table
+          (let [{col-name :name :as column} (column-keyword table-columns)]
+            (assert column
+                    (str "Unknown column in where clause: no "
+                         column-keyword " in table " table))
+            (if-let [composite-columns (and
+                                        (map? value)
+                                        (some->> column :type
+                                                 (composite-type table-info-registry)
+                                                 table-info-registry
+                                                 :columns))]
+              ;; composite type: add all fields as separate clauses
+              (reduce (fn [where [kw val]]
+                        (assert (composite-columns kw)
+                                (str "Unknown column in where clause: no "
+                                     kw " in composite type "
+                                     (composite-type table-info-registry (:type column))))
+                        (add-where where
+                                   (composite-columns kw)
+                                   (str "(" alias ".\"" col-name "\").\""
                                              (:name (composite-columns kw)) "\"")
-                                        kw val))
-                           where value)
+                                   kw val))
+                      where value)
 
-                   ;; normal column
-                   (add-where where (table-columns column-keyword)
-                              (str alias ".\"" col-name "\"") column-keyword value)))))
-           {:clause [] :parameters []}
-           record) w
-      (update w :clause #(str/join " AND " %))
-      ((juxt :clause :parameters) w))))
+              ;; normal column
+              (add-where where (table-columns column-keyword)
+                         (str alias ".\"" col-name "\"") column-keyword value)))))
+      {:clause [] :parameters []}
+      record))))
 
 (defn sql-where
   ([table-info-registry path->table record]
